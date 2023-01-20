8bde672774bae4990318f8510dc7fc2ea0210d78
Stepan <shtefff@gmail.com>
2023-01-21 05:06:07 +0700

    sber log

--- app/Models/Sber.php
+++ app/Models/Sber.php
@@ -27,7 +27,7 @@ class Sber
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         $response = curl_exec($ch);
         curl_close($ch);
-
+        Log::error(json_decode($response));
         return json_decode($response);
     }
 
